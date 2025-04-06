1e630d7be42604b28ed89e6b5ef1f525a3ab5c2e
Shashank Kamoji <shashankkamoji@gmail.com>
2025-04-06 20:23:43 +0530

    profile edit, posts delete, profile view feat added

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,10 @@
 const User = require("../models/user");
 const Blog = require("../models/blog");
+const path = require("path");
+const fs = require("fs");
+const { createHmac, randomBytes } = require("crypto");
+
+
 
 
 const handleGetSignin = (req, res) => {    return res.render("signin") };
@@ -39,6 +44,66 @@ const handleGetMyPosts = async (req, res) => {
 };
 
 
+const handleUpdateProfileImage = async(req, res) => {
+  const user = await User.findById(req.user._id);
+  if(user.profileImageURL !== "/images/default.png" ) {
+    const filePath = path.join(process.cwd(), "public", user.profileImageURL);
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.error("Error deleting file:", err);
+        } 
+      });
+  }
+  await User.updateOne({ _id: req.user._id }, { $set: { profileImageURL: `/images/${req.file.filename}` } });
+  res.render("editprofile", {
+    user: req.user,
+  });
+
+}
+
+const handleEditProfile = (req,res) => {
+  res.render("editprofile", {
+    user: req.user,
+  });
+}
+
+const handleUpdateUserName = async (req, res) => {
+  if(!req.body || !req.body.username ) {
+    return res.render("editprofile", {
+      user: req.user,
+      error: "please enter username",
+    });
+  }
+  await User.updateOne({ _id: req.user._id }, { $set: { fullName: req.body.username } });
+  req.user = {
+    ...req.user,
+    fullName: req.body.username,
+  }
+  res.render("editprofile", {
+    user: req.user,
+  });  
+
+}
+
+const handleUpdateUserPassword = async (req, res) => {
+  if(!req.body || !req.body.password ) {
+    return res.render("editprofile", {
+      user: req.user,
+      error: "please enter pasword",
+    });
+  }
+  const saltt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", saltt)
+    .update(req.body.password)
+    .digest("hex");
+
+  await User.updateOne({ _id: req.user._id }, { $set: { salt: saltt, password: hashedPassword  } });
+  res.render("editprofile", {
+    user: req.user,
+  });  
+
+}
+
 
 const handlePostSignup =  async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -50,6 +115,22 @@ const handlePostSignup =  async (req, res) => {
   return res.redirect("/");
 }
 
+const getUserDetails = async (req, res) => {
+  const userdetails = await User.findById(req.params.id);
+  const userfound = { 
+    fullName: userdetails.fullName ,
+    email: userdetails.email,
+    role: userdetails.role,
+    profileImageURL: userdetails.profileImageURL,
+  }
+  const userBlogs = await Blog.find({ createdBy : req.params.id});
+  res.render("viewprofile",{
+    blogs: userBlogs,
+    userdetails: userfound,
+    user: req.user,
+  });
+}
+
 module.exports = {
     handleGetSignin,
     handleGetSignup,
@@ -57,4 +138,9 @@ module.exports = {
     handleLogout,
     handleGetMyPosts,
     handlePostSignup,
+    handleEditProfile,
+    handleUpdateProfileImage,
+    handleUpdateUserName,
+    handleUpdateUserPassword,
+    getUserDetails,
 };
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,11 @@
 const { Router } = require("express");
+const multer = require("multer");
+const path = require("path");
+const sharp = require("sharp");
+const fs = require("fs");
+
+
+
 const  {
   handleGetSignin,
   handleGetSignup,
@@ -6,9 +13,54 @@ const  {
   handleLogout,
   handleGetMyPosts,
   handlePostSignup,
+  handleEditProfile,
+  handleUpdateProfileImage,
+  handleUpdateUserName,
+  handleUpdateUserPassword,
+  getUserDetails,
 
 } = require("../controllers/user")
+
 const router = Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(`./public/images/`));
+  },
+  filename: function (req, file, cb) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+
+
+const cropImgage = (req, res, next) => {
+  const inputPath = path.join(process.cwd(), "public", "images", req.file.filename );
+  const tempPath = path.join(process.cwd(), "public", "images", "input-temp.jpg");
+  sharp(inputPath)
+  .metadata()
+  .then(({ width, height }) => {
+    const size = Math.min(width, height);
+    const left = Math.floor((width - size) / 2);
+    const top = Math.floor((height - size) / 2);
+
+    return sharp(inputPath)
+      .extract({ width: size, height: size, left, top })
+      .toFile(tempPath);
+  })
+  .then(() => {
+    fs.renameSync(tempPath, inputPath);
+    next();
+  })
+  .catch(err => {
+    console.error(" Error processing image:", err);
+  });
+}
+
+
 
 router.get("/signin", handleGetSignin);
 
@@ -20,6 +72,17 @@ router.get("/logout", handleLogout);
 
 router.get("/myposts", handleGetMyPosts);
 
+router.get("/editprofile", handleEditProfile);
+
+router.post("/updateProfileimg",upload.single("profileImage"), cropImgage, handleUpdateProfileImage);
+
+router.post("/updateUserName", handleUpdateUserName);
+
+router.post("/updateUserPassword", handleUpdateUserPassword);
+
 router.post("/signup",handlePostSignup);
+
+router.get("/:id", getUserDetails);
+
 
 module.exports = router;
